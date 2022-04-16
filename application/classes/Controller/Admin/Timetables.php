a015de3cf69a83403bc497e0d7056ef700def7d6
aleksander_drynda <alekdry203@student.polsl.pl>
2022-04-17 00:45:35 +0200

    admin okienka - wersja prototypowa

--- application/classes/Controller/Admin/Timetables.php
+++ application/classes/Controller/Admin/Timetables.php
@@ -6,60 +6,76 @@ class Controller_Admin_Timetables extends Controller_Admin_Main {
 		$timetables=ORM::factory('Timetable');
 		//print_r($timetables);die();
 		if(@$_GET) $this->filter($timetables);
-		$data['timetables']=$timetables->group_by(DB::expr('DATE(vaccination_date)'))->group_by('users_id')->order_by(DB::expr('DATE(vaccination_date)'))->find_all();
-		$data['users']=ORM::factory();
-		$this->template->content=View::factory("admin/timetables/index");
+		$data['timetables']=$timetables->group_by(DB::expr('DATE(vaccination_date)'))
+										->group_by('users_id')
+										->order_by(DB::expr('DATE(vaccination_date)'))
+										->select(array(DB::expr('DATE(vaccination_date)'), 'day'))
+										->find_all();
+		$data['users']=ORM::factory('User')->order_by('name')->order_by('surname')->find_all();
+		$this->template->content=View::factory("admin/timetables/index", $data);
 	}
 	
 	private function filter($timetables){
 		//print_r($_GET);die();
-		if(@$_GET['date'][0]) $timetables->where('vaccination_date', '>=', $_GET['date'][0]);
-		if(@$_GET['date'][1]) $timetables->where('vaccination_date', '<=', $_GET['date'][1]);
+		if(@$_GET['date'][0]) $timetables->where('vaccination_date', '>=', $_GET['date'][0].' 00:00:00');
+		if(@$_GET['date'][1]) $timetables->where('vaccination_date', '<=', $_GET['date'][1].' 23:59:59');
 		if(@$_GET['user_id']) $timetables->where('users_id', '=', $_GET['user_id']);
-		if(@$_GET['status']==1) $timetables->where('patients_pesel', 'is', null);
+		/*if(@$_GET['status']==1) $timetables->where('patients_pesel', 'is', null);
 		elseif(@$_GET['status']==2) $timetables->where('patients_pesel', 'is not', null)->where('payment', 'is', null);
-		elseif(@$_GET['status']==3) $timetables->where('payment', 'is not', null);
-	}
-	
-	public function action_add(){
-		if(@$_POST) $this->save_add();
-		//$data['timetable']=ORM::factory('Timetable');
-		$this->template->content=View::factory("admin/timetables/add");
-	}
-	
-	private function save_add(){
-		print_r($_POST);die();
-		$time=strtotime($_POST['date'].' '.$_POST['hour']);
-		for($i=0; $i<=$_POST['amount']; $i++){
-			$tmp=date('Y-m-d H:i:s', $time+($i*$_POST['period']));
-			echo $tmp;die();
-			$timetable=ORM::factory('Timetable');
-			$timetable->vaccination_date=$tmp;
-			$timetable->users_id=$_POST['user_id'];
-			$timetable->save();
-		}
-		HTTP::redirect("admin/timetables/edit/".$_POST['user_id']."?date=".$_POST['date']);
+		elseif(@$_GET['status']==3) $timetables->where('payment', 'is not', null);//*/
 	}
 	
 	public function action_edit(){
-		if(@$_POST) $this->save_edit();
-		$data['timetable']=ORM::factory('Timetable', $this->request->param("id"));
-		$this->template->content=View::factory("admin/timetables/edit");
+		if(@$_POST) $this->save();
+		$timetables=ORM::factory('Timetable');
+		if(@$_GET['date']) $timetables->where('vaccination_date', '>=', @$_GET['date'].' 00:00:00')
+										->where('vaccination_date', '<=', @$_GET['date'].' 23:59:59');
+		else $timetables->or_where('id', 'in', array(0));
+		if($this->request->param("id")) $timetables->where('users_id', '=', $this->request->param("id"));
+		//$data['timetables']=ORM::factory('Timetable')->select(array(DB::expr('DATE(vaccination_date)'), 'day'))->find_all();
+		$data['timetables']=$timetables->order_by(DB::expr('DATE(vaccination_date)'))
+										->select(array(DB::expr('DATE(vaccination_date)'), 'day'))
+										->find_all();//*/
+		$data['users']=ORM::factory('User')->order_by('name')->order_by('surname')->find_all();
+		$this->template->content=View::factory("admin/timetables/edit", $data);
 	}
 	
-	private function save_edit(){
-		print_r($_POST);die();
-		$timetable=ORM::factory('Timetable', @$_POST['id']);
-		$timetable->name=$_POST['name'];
-		$timetable->surname=$_POST['surname'];
-		$timetable->login=$_POST['login'];
-		$timetable->admin=@$_POST['admin'] ? : null;
-		if(@$_POST['password']) $timetable->admin=$this->pass_hash($_POST['password']);
-		$timetable->save();
+	private function save(){
+		//print_r($_POST);die();
+		foreach($_POST['id'] as $i=>$val){
+			$timetable=ORM::factory('Timetable', $val);
+			$timetable->vaccination_date=$_POST['vaccination_date_day'][$i].' '.$_POST['vaccination_date_time'][$i];
+			$timetable->users_id=$_POST['user_id'];
+			$timetable->patients_pesel=$_POST['patients_pesel'][$i] ? : null;
+			$timetable->payment=$_POST['payment'][$i] ? : null;
+			$timetable->activation_code=$_POST['activation_code'][$i] ? : null;
+			$timetable->save();
+		}
+		HTTP::redirect("admin/timetables/edit/".$_POST['user_id']."?date=".@$_POST['vaccination_date_day'][0]);
 	}
 	
 	public function action_delete(){
-		ORM::factory('Timetable', $this->request->param("id"))->delete();
+		$timetable=ORM::factory('Timetable', $this->request->param("id"));
+		//$redirect="admin/timetables/edit/".$timetable->users_id.'?date='.date('Y-m-d', strtotime($timetable->vaccination_date));
+		if(!$timetable->patients_pesel) $timetable->delete();
+		elseif($timetable->patients_pesel && !$timetable->payment) die('wysłać powiadomienie o usunięciu wizyty z propozycją nowej daty?');
+		elseif($timetable->patients_pesel && $timetable->payment) die('nie można usunąć zrealizowanego szczepienia');
+		else die('nieznany przypadek!!!');
+		HTTP::redirect("admin/timetables");
+		//HTTP::redirect($redirect);
+	}
+	
+	public function action_delete_day(){
+		$timetables=ORM::factory('Timetable')->where('vaccination_date', '>=', @$_GET['date'].' 00:00:00')
+											->where('vaccination_date', '<=', @$_GET['date'].' 23:59:59')
+											->where('users_id', '=', $this->request->param("id"))
+											->find_all();
+		foreach($timetables as $timetable){
+			if(!$timetable->patients_pesel) $timetable->delete();
+			elseif($timetable->patients_pesel && !$timetable->payment) die('wysłać powiadomienie o usunięciu wizyty z propozycją nowej daty?');
+			elseif($timetable->patients_pesel && $timetable->payment) die('nie można usunąć zrealizowanego szczepienia');
+			else die('nieznany przypadek!!!');
+		}
 		HTTP::redirect("admin/timetables");
 	}
 	
--- application/views/admin/timetables/index.php
+++ application/views/admin/timetables/index.php
@@ -2,27 +2,28 @@
 	<div class="searchBox">
 		<div class="searchFormEl">
 			<label>Data:</label>
-			<input type="date" name="date[]" value="<?= @$_GET['date'][0] ?>" />
+			<input type="date" name="date[0]" value="<?= @$_GET['date'][0] ?>" />
 			 - 
-			<input type="date" name="date[]" value="<?= @$_GET['date'][1] ?>" />
+			<input type="date" name="date[1]" value="<?= @$_GET['date'][1] ?>" />
+			<span class="error dateError">Data od nie może być większa niż data do!</span>
 		</div>
 		<div class="searchFormEl">
 			<label>Lekarz:</label>
 			<select name="user_id">
 				<option value="">wybierz</option>
 				<? foreach($users as $user): ?>
-					<option value=""><?= $user->name.' '.$user->surname ?></option>
+					<option value="<?= $user->id ?>"><?= $user->name.' '.$user->surname ?></option>
 				<? endforeach ?>
 			</select>
 		</div>
-		<div class="searchFormEl">
+		<!--div class="searchFormEl">
 			<label>Status:</label>
 			<input type="radio" name="status" value="1" <?= @$_GET['status']==1 ? 'checked' : null ?> /> wolne
 			<input type="radio" name="status" value="2" <?= @$_GET['status']==2 ? 'checked' : null ?> /> zarezerwowane
-			<input type="radio" name="status" value="3" <?= @$_GET['status']==3 ? 'checked' : null ?> /> zakończone
-		</div>
+			<input type="radio" name="status" value="3" <?= @$_GET['status']==3 ? 'checked' : null ?> /> zrealizowane
+		</div-->
 		<input type="submit" class="button" value="Szukaj" />
-		<a href="<?= URL::base() ?>index.php/admin/timetable/add" class="linkButton">dodaj</a>
+		<a href="<?= URL::base() ?>index.php/admin/timetables/edit" class="linkButton">dodaj</a>
 	</div>
 <?= form::close() ?>
 
@@ -31,21 +32,59 @@
 		<tr>
 			<th>Dzień</th>
 			<th>Lekarz</th>
-			<th>Status</th>
+			<!-->Status</th-->
 			<th>Operacje</th>
 		</tr>
 	</thead>
 	<tbody>
 		<? foreach($timetables as $timetable): ?>
 			<tr>
-				<td><?= $user->day ?></td>
-				<td><?= $user->login ?></td>
-				<td><?= $user->admin ? 'tak' : 'nie' ?></td>
+				<td><?= $timetable->day ?></td>
+				<td><?= $timetable->user->name.' '.$timetable->user->surname ?></td>
+				<!--td>
+					<?
+						//if($timetable->patient_pesel) echo 'wolne'
+					?>
+				</td-->
 				<td>
-					<a href="<?= URL::base() ?>index.php/admin/users/edit<?= $user->id ?>" class="linkButton">edytuj</a>
-					<a href="<?= URL::base() ?>index.php/admin/users/delete<?= $user->id ?>" class="linkButton">usuń</a>
+					<a href="<?= URL::base() ?>index.php/admin/timetables/edit/<?= $timetable->user->id.'?date='.$timetable->day ?>" class="linkButton">edytuj</a>
+					<a href="<?= URL::base() ?>index.php/admin/timetables/delete_day/<?= $timetable->user->id.'?date='.$timetable->day ?>"  onclick="return confirm('Na pewno chcesz usunąć?')" class="linkButton">usuń</a>
 				</td>
 			</tr>
 		<? endforeach ?>
 	</tbody>
 </table>
+
+<script>
+	$(document).ready(function(){
+		// data 0 nie może być większa od 1
+		$('input[name="date[0]"]').change(function(){
+			check_dates();
+		});
+		$('input[name="date[1]"]').change(function(){
+			check_dates();
+		});
+		function check_dates(){
+			var date0=$('input[name="date[0]"]').val();
+			var date1=$('input[name="date[1]"]').val();
+			if(date0 && date1 && date0>date1){
+				$('input[name="date[1]"]').val(date0)
+				$('.dateError').show();
+			}else{
+				$('.dateError').hide();
+			}
+		}
+		
+		/*
+		// zerowanie wyboru statusu
+		var status=$('input[name=status]').val() ? $('input[name=status]').val() : 0;
+		$('input[name=status]').click(function(){
+			if(status==$(this).val()){
+				status=0;
+				$(this).prop('checked', false);
+			}else{
+				status=$(this).val();
+			}
+		});//*/
+	});
+</script>
