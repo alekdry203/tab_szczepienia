05f7042eb7ee71ecd0b17f4f4d3f81bdeca2476a
aleksander_drynda <alekdry203@student.polsl.pl>
2022-05-08 08:28:40 +0200

    dodanie generowania pdf z widoku

---
+++ application/views/welcome/test_pdf.php
@@ -0,0 +1,19 @@
+<?
+	//$php_css_class="style='border: 1px solid;'";
+?>
+<h1>Przykładowy nagłówek</h1>
+
+<table style='/*width: 100%;border-collapse: collapse;border: 1px solid;*/'>
+	<thead>
+		<tr <?//= $php_css_class ?>>
+			<th <?//= $php_css_class ?>>Nagłówek</th>
+			<th <?//= $php_css_class ?>>Tabeli</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr <?//= $php_css_class ?>>
+			<td <?//= $php_css_class ?>>Przykładowy</td>
+			<td <?//= $php_css_class ?>>tekst</td>
+		</tr>
+	</tbody>
+</table>
