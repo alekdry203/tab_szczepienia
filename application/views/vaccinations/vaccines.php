c3dbe5e530ad5f9b26384ba8daf233147f659953
aleksander_drynda <alekdry203@student.polsl.pl>
2022-05-13 00:25:55 +0200

    pacjent - szczepionki - usunięcie dubli

--- application/views/vaccinations/vaccines.php
+++ application/views/vaccinations/vaccines.php
@@ -12,14 +12,5 @@
 				</a>
 			</div>
 		</div>
-		<?//*/?><div class="vaccineBox">
-			<label><?= $vaccine->name ?></label>
-			(producent <?= $vaccine->producer ?>)
-			<div class="vaccineButtonBox">
-				<a href="<?= URL::base().'index.php/vaccinations/index?vaccine='.$vaccine->name.';'.$vaccine->producer ?>">
-					<span class="vaccineButton">zapisz się na szczepienie</span>
-				</a>
-			</div>
-		</div><?//*/?>
 	<? endforeach ?>
 </div>
